51da66ff9e5f675e38cac7efff537d9d64d9fcd0
John Bell <john@thebiggerboat.co.uk>
2023-07-12 21:50:00 +0100

    "Fix conditional logic for inviting unconfirmed users"

--- resources/views/livewire/admin/user/show.blade.php
+++ resources/views/livewire/admin/user/show.blade.php
@@ -14,7 +14,7 @@
         </div>
         <div
             class="justify-stretch mt-6 flex flex-col-reverse space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-y-0 sm:space-x-3 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3">
-            @if (!$user->confirmed && $user->role == 'captain')
+            @if (!$user->confirmed)
                 <button type="button" 
                     wire:click="invite()"
                     wire:loading.attr="disabled"
