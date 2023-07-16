f60aba6a80b4a9e57e2e9139f02d6e14a6945cab
John Bell <john@thebiggerboat.co.uk>
2023-07-16 11:05:42 +0100

    Update footer background color to neutral-800

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-gray-900" aria-labelledby="footer-heading">
+<footer class="bg-neutral-800" aria-labelledby="footer-heading">
     <h2 id="footer-heading" class="sr-only">Footer</h2>
     <div class="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8 lg:py-32">
       <div class="xl:grid xl:grid-cols-3 xl:gap-8">
