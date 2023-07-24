b1109f4c634271291751b13cae1bb5f09238433f
John Bell <john@thebiggerboat.co.uk>
2023-07-24 16:23:27 +0100

    Add pooltableguru.png and ukplasticsandglazing-logo.jpeg as new sponsor images and update sponsor links in logo-clouds.blade.php

--- resources/views/components/logo-clouds.blade.php
+++ resources/views/components/logo-clouds.blade.php
@@ -1,9 +1,9 @@
 <div class="bg-white py-24 sm:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <h2 class="text-center text-lg font-semibold leading-8 text-gray-900">Proudly sponsored by</h2>
-      <div class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-        <a href="https://www.billiardsupply.co.uk/" target="_blank" rel="noopener noreferrer" class="col-span-2 max-h-20 w-full object-contain lg:col-span-1">
-          <img class="col-span-2 max-h-20 w-full object-contain lg:col-span-1" src="{{ asset('images/sponsors/billiard-supply-logo.png') }}" alt="Billiard Supply" width="158" height="48">
+      <div class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-6">
+        <a href="https://www.thepooltableguru.co.uk/" target="_blank" rel="noopener noreferrer" class="col-span-2 w-full object-contain lg:col-span-1">
+          <img class="col-span-2 max-h-20 w-full object-contain lg:col-span-1" src="{{ asset('images/sponsors/pooltableguru.png') }}" alt="The Pool Table Guru" width="158" height="48">
         </a>
         <a href="https://www.eagle-roofing.co.uk/" target="_blank" rel="noopener noreferrer" class="col-span-2 max-h-20 w-full object-contain lg:col-span-1">
           <img class="col-span-2 max-h-20 w-full object-contain lg:col-span-1" src="{{ asset('images/sponsors/eagleroofing-logo.png') }}" alt="Eagle Roofing" width="158" height="48">
@@ -16,6 +16,9 @@
         </a>
         <a href="https://www.levelshuddersfield.co.uk/" target="_blank" rel="noopener noreferrer" class="col-span-4 max-h-20 w-full object-contain lg:col-span-1">
           <img class="col-span-2 max-h-20 w-full object-contain lg:col-span-1" src="{{ asset('images/sponsors/levelshuddersfield.svg') }}" alt="Levels Huddersfield" width="158" height="48">
+        </a>
+        <a href="https://www.facebook.com/ukplasticsandglazingltd" target="_blank" rel="noopener noreferrer" class="col-span-4 max-h-20 w-full object-contain lg:col-span-1">
+          <img class="col-span-2 max-h-20 w-full object-contain lg:col-span-1" src="{{ asset('images/sponsors/ukplasticsandglazing-logo.jpeg') }}" alt="UK Plastics & Glazing Ltd" width="158" height="48">
         </a>
       </div>
     </div>
