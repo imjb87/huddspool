3f3ef1f90bf3ea1cd6180aff0e55b9d1820d6196
John Bell <john@thebiggerboat.co.uk>
2023-08-21 10:40:24 +0100

    fix google tag manager

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,50 +1,59 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta property="og:title" content="Huddersfield & District Tuesday Night Pool League" />
-        <meta property="og:description" content="Every Tuesday night, teams from across Huddersfield and the surrounding areas compete in the Huddersfield & District Tuesday Night Pool League. Check here for the latest tables, fixtures, results and averages." />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.huddspool.co.uk" />
-        <meta property="og:image" content="{{ asset('images/og-image.jpg') }}" />
 
-        <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/png">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta property="og:title" content="Huddersfield & District Tuesday Night Pool League" />
+    <meta property="og:description"
+        content="Every Tuesday night, teams from across Huddersfield and the surrounding areas compete in the Huddersfield & District Tuesday Night Pool League. Check here for the latest tables, fixtures, results and averages." />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://www.huddspool.co.uk" />
+    <meta property="og:image" content="{{ asset('images/og-image.jpg') }}" />
 
-        @if (Route::currentRouteName() == 'result.create')
-            <meta http-equiv="refresh" content="600">
-        @endif
+    <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/png">
 
-        <title>{{ config('app.name', 'Huddersfield & District Tuesday Night Pool League') }}</title>
+    @if (Route::currentRouteName() == 'result.create')
+        <meta http-equiv="refresh" content="600">
+    @endif
 
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-      
-            gtag('config', 'G-620MNWY15S');
-        </script>    
+    <title>{{ config('app.name', 'Huddersfield & District Tuesday Night Pool League') }}</title>
 
-        <!-- Fonts -->
-        <script src="https://kit.fontawesome.com/b12bfcfdee.js" crossorigin="anonymous"></script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-620MNWY15S"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-        <!-- Scripts -->
-        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
-        @livewireStyles
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @laravelPWA
-    </head>
-    <body class="font-sans antialiased [&_[x-cloak]]:hidden">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-            @include('layouts.footer')
-            <livewire:search />
-        </div>
-        @livewireScripts
-    </body>
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-620MNWY15S');
+    </script>
+
+    <!-- Fonts -->
+    <script src="https://kit.fontawesome.com/b12bfcfdee.js" crossorigin="anonymous"></script>
+
+    <!-- Scripts -->
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    @livewireStyles
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @laravelPWA
+</head>
+
+<body class="font-sans antialiased [&_[x-cloak]]:hidden">
+    <div class="min-h-screen bg-gray-100">
+        @include('layouts.navigation')
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+        @include('layouts.footer')
+        <livewire:search />
+    </div>
+    @livewireScripts
+</body>
+
 </html>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,32 +1,48 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <script src="https://kit.fontawesome.com/b12bfcfdee.js" crossorigin="anonymous"></script>
-        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @laravelPWA
-    </head>
-    <body class="font-sans text-gray-900 antialiased [&_[x-cloak]]:hidden">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 px-4">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-620MNWY15S"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-            <div>
-                <a href="/">
-                    <img class="w-36" src="{{ asset('images/logo.png') }}" alt="Huddersfield Pool Logo" />
-                </a>
-            </div>
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden rounded-lg">
-                {{ $slot }}
-            </div>
+        gtag('config', 'G-620MNWY15S');
+    </script>
+
+    <!-- Fonts -->
+    <script src="https://kit.fontawesome.com/b12bfcfdee.js" crossorigin="anonymous"></script>
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @laravelPWA
+</head>
+
+<body class="font-sans text-gray-900 antialiased [&_[x-cloak]]:hidden">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 px-4">
+
+        <div>
+            <a href="/">
+                <img class="w-36" src="{{ asset('images/logo.png') }}" alt="Huddersfield Pool Logo" />
+            </a>
         </div>
-    </body>
+
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden rounded-lg">
+            {{ $slot }}
+        </div>
+    </div>
+</body>
+
 </html>
