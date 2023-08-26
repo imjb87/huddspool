4c4f26580efce0c02a692bbc4376f0d6aa90f86d
John Bell <john@thebiggerboat.co.uk>
2023-08-27 00:28:09 +0100

    refactor table view on mobile

--- resources/views/livewire/table/index.blade.php
+++ resources/views/livewire/table/index.blade.php
@@ -18,61 +18,69 @@
                                 <div class="w-full max-w-full overflow-hidden">
                                     <div class="bg-gray-50">
                                         <div class="flex">
-                                            <div scope="col"
-                                                class="px-4 py-2 text-left text-sm font-semibold text-gray-900 w-[32px] md:w-1/12">
-                                                #
+                                            <div class="flex w-6/12">
+                                                <div scope="col"
+                                                    class="px-4 py-2 text-left text-sm font-semibold text-gray-900 w-2/12">
+                                                    #
+                                                </div>
+                                                <div scope="col"
+                                                    class="px-2 py-2 sm:px-3 text-left text-sm font-semibold text-gray-900 w-10/12 truncate">
+                                                    Team</div>
                                             </div>
-                                            <div scope="col"
-                                                class="px-2 py-2 sm:px-3 text-left text-sm font-semibold text-gray-900 flex-grow flex-shrink truncate">
-                                                Team</div>
-                                            <div scope="col"
-                                                class="py-2 text-center text-sm font-semibold text-gray-900 w-[32px] md:w-1/12">Pl
+                                            <div class="flex w-6/12">
+                                                <div scope="col"
+                                                    class="py-2 text-center text-sm font-semibold text-gray-900 w-1/5">Pl
+                                                </div>
+                                                <div scope="col"
+                                                    class="py-2 text-center text-sm font-semibold text-gray-900 w-1/5">W
+                                                </div>
+                                                <div scope="col"
+                                                    class="py-2 text-center text-sm font-semibold text-gray-900 w-1/5">D</div>
+                                                <div scope="col"
+                                                    class="py-2 text-center text-sm font-semibold text-gray-900 w-1/5">L</div>
+                                                <div scope="col"
+                                                    class="py-2 text-center text-sm font-semibold text-gray-900 w-1/5">Pts</div>
                                             </div>
-                                            <div scope="col"
-                                                class="py-2 text-center text-sm font-semibold text-gray-900 w-[32px] md:w-1/12">W
-                                            </div>
-                                            <div scope="col"
-                                                class="py-2 text-center text-sm font-semibold text-gray-900 w-[32px] md:w-1/12">D</div>
-                                            <div scope="col"
-                                                class="py-2 text-center text-sm font-semibold text-gray-900 w-[32px] md:w-1/12">L</div>
-                                            <div scope="col"
-                                                class="py-2 text-center text-sm font-semibold text-gray-900 w-[32px] md:w-1/12">Pts</div>
-                                        </tr>
+                                        </div>
                                     </div>
                                     <div class="bg-white">
                                         @foreach ($section->standings() as $team)
                                             <a href="{{ route('team.show', $team->id) }}" class="border-t border-gray-300 hover:cursor-pointer hover:bg-gray-50 flex">
-                                                <div
-                                                    class="whitespace-nowrap py-2 px-4 text-sm font-medium text-gray-900 text-left w-[32px] md:w-1/12">
-                                                    {{$loop->iteration}}.
+                                                <div class="flex w-6/12">
+                                                    <div
+                                                        class="whitespace-nowrap py-2 px-4 text-sm font-medium text-gray-900 text-left w-2/12">
+                                                        {{$loop->iteration}}.
+                                                    </div>
+                                                    <div
+                                                        class="tooltip tooltip-top px-2 sm:px-3 py-2 text-sm text-gray-900 w-10/12 truncate {{ $team->pivot->withdrawn_at ? 'line-through' : '' }}">
+                                                        <span class="{{ $team->shortname ? "hidden md:inline" : "" }}">{{ $team->name }}</span>
+                                                        @if ($team->shortname)
+                                                            <span class="md:hidden" href="{{ route('team.show', $team->id) }}">{{ $team->shortname }}</span>
+                                                        @endif
+                                                        @if ($team->pivot->withdrawn_at)
+                                                        <span class="tooltip-text">
+                                                            This team was withdrawn from the section on {{ date('d/m/Y', strtotime($team->pivot->withdrawn_at)) }}
+                                                        </span>
+                                                        @endif        
+                                                    </div>
                                                 </div>
-                                                <div
-                                                    class="tooltip tooltip-top px-2 sm:px-3 py-2 text-sm text-gray-900 flex-grow truncate {{ $team->pivot->withdrawn_at ? 'line-through' : '' }}">
-                                                    <span class="{{ $team->shortname ? "hidden md:inline" : "" }}">{{ $team->name }}</span>
-                                                    @if ($team->shortname)
-                                                        <span class="md:hidden" href="{{ route('team.show', $team->id) }}">{{ $team->shortname }}</span>
-                                                    @endif
-                                                    @if ($team->pivot->withdrawn_at)
-                                                    <span class="tooltip-text">
-                                                        This team was withdrawn from the section on {{ date('d/m/Y', strtotime($team->pivot->withdrawn_at)) }}
-                                                    </span>
-                                                    @endif        
+                                                <div class="flex w-6/12">
+                                                    <div
+                                                        class="whitespace-nowrap py-2 text-sm text-gray-900 font-semibold text-center w-1/5">
+                                                        {{$team->played}}</div>
+                                                    <div
+                                                        class="whitespace-nowrap py-2 text-sm text-gray-900 font-semibold text-center w-1/5">
+                                                        {{$team->wins}}</div>
+                                                    <div
+                                                        class="whitespace-nowrap py-2 text-sm text-gray-900 font-semibold text-center w-1/5">
+                                                        {{$team->draws}}</div>
+                                                    <div
+                                                        class="whitespace-nowrap py-2 text-sm text-gray-900 font-semibold text-center w-1/5">
+                                                        {{$team->losses}}</div>
+                                                    <div
+                                                        class="whitespace-nowrap py-2 text-sm text-gray-900 font-semibold text-center w-1/5">
+                                                        {{$team->points}}</div>
                                                 </div>
-                                                <div
-                                                    class="whitespace-nowrap py-2 text-sm text-gray-900 font-semibold text-center w-[32px] md:w-1/12">
-                                                    {{$team->played}}</div>
-                                                <div
-                                                    class="whitespace-nowrap py-2 text-sm text-gray-900 font-semibold text-center w-[32px] md:w-1/12">
-                                                    {{$team->wins}}</div>
-                                                <div
-                                                    class="whitespace-nowrap py-2 text-sm text-gray-900 font-semibold text-center w-[32px] md:w-1/12">
-                                                    {{$team->draws}}</div>
-                                                <div
-                                                    class="whitespace-nowrap py-2 text-sm text-gray-900 font-semibold text-center w-[32px] md:w-1/12">
-                                                    {{$team->losses}}</div>
-                                                <div
-                                                    class="whitespace-nowrap py-2 text-sm text-gray-900 font-semibold text-center w-[32px] md:w-1/12">
-                                                    {{$team->points}}</div>
                                             </a>
                                         @endforeach
                                     </div>
