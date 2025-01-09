12743037494432b37f8d22ec27d16f2bb5a7e8e4
John Bell <john@thebiggerboat.co.uk>
2025-01-09 12:30:22 +0000

    Update sponsor image to thepooltableguru.jpg

--- resources/views/components/logo-clouds.blade.php
+++ resources/views/components/logo-clouds.blade.php
@@ -3,7 +3,7 @@
       <h2 class="text-center text-lg font-semibold leading-8 text-gray-900">Proudly sponsored by</h2>
       <div class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-6">
         <a href="https://www.thepooltableguru.co.uk/" target="_blank" rel="noopener noreferrer" class="col-span-2 w-full object-contain lg:col-span-1">
-          <img class="col-span-2 max-h-20 w-full object-contain lg:col-span-1 px-3" src="{{ asset('images/sponsors/pooltableguru.png') }}" alt="The Pool Table Guru" width="158" height="48">
+          <img class="col-span-2 max-h-20 w-full object-contain lg:col-span-1 px-3" src="{{ asset('images/sponsors/thepooltableguru.jpg') }}" alt="The Pool Table Guru" width="158" height="48">
         </a>
         <a href="https://www.eagle-roofing.co.uk/" target="_blank" rel="noopener noreferrer" class="col-span-2 max-h-20 w-full object-contain lg:col-span-1">
           <img class="col-span-2 max-h-20 w-full object-contain lg:col-span-1 px-3" src="{{ asset('images/sponsors/eagleroofing-logo.png') }}" alt="Eagle Roofing" width="158" height="48">
