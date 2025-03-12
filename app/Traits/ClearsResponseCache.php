7f9576c31a02db1e208bcb2ea3853700a750afde
John Bell <john@thebiggerboat.co.uk>
2025-03-12 06:04:55 +0000

    Update cache clearing path for player averages

--- app/Traits/ClearsResponseCache.php
+++ app/Traits/ClearsResponseCache.php
@@ -11,19 +11,19 @@ trait ClearsResponseCache
         self::created(function () {
             ResponseCache::forget('/tables/{$this->ruleset->slug}/');
             ResponseCache::forget('/fixtures-and-results/{$this->ruleset->slug}/');
-            ResponseCache::forget('/averages/{$this->ruleset->slug}/');
+            ResponseCache::forget('/players/averages/{$this->ruleset->slug}/');
         });
 
         self::updated(function () {
             ResponseCache::forget('/tables/{$this->ruleset->slug}/');
             ResponseCache::forget('/fixtures-and-results/{$this->ruleset->slug}/');
-            ResponseCache::forget('/averages/{$this->ruleset->slug}/');
+            ResponseCache::forget('/players/averages/{$this->ruleset->slug}/');
         });
 
         self::deleted(function () {
             ResponseCache::forget('/tables/{$this->ruleset->slug}/');
             ResponseCache::forget('/fixtures-and-results/{$this->ruleset->slug}/');
-            ResponseCache::forget('/averages/{$this->ruleset->slug}/');
+            ResponseCache::forget('/players/averages/{$this->ruleset->slug}/');
         });
     }
 }
