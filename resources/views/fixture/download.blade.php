71b2038b7595217a6ce8c0a99dabd9b20aa87821
John Bell <john@thebiggerboat.co.uk>
2025-08-05 13:29:27 +0100

    Add Open Graph meta tags and favicon link in head section

--- resources/views/fixture/download.blade.php
+++ resources/views/fixture/download.blade.php
@@ -4,8 +4,16 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta property="og:title" content="Huddersfield & District Tuesday Night Pool League" />
+    <meta property="og:description"
+        content="Every Tuesday night, teams from across Huddersfield and the surrounding areas compete in the Huddersfield & District Tuesday Night Pool League. Check here for the latest tables, fixtures, results and averages." />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://www.huddspool.co.uk" />
+    <meta property="og:image" content="{{ asset('images/og-image.jpg') }}" />
     <title>{{ $section->name }} Fixtures | Huddersfield &amp; District Tuesday Night Pool League</title>
+
+    <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/png">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
