cd26a1ab4d5dbc46a4fe37badf2a60a16d3d93d9
John Bell <john@thebiggerboat.co.uk>
2026-03-22 19:18:18 +0000

    logo tweak

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,6 +1,6 @@
 <span class="relative fa-stack -ml-1 top-1" aria-hidden="true">
     <i class="fa-solid fa-circle fa-stack-2x text-red-500"></i>
     <i class="fa-solid fa-circle fa-stack-2x text-yellow-500 ml-2 -mt-2"></i>
-    <i class="fa-solid fa-circle fa-stack-2x text-white ml-4"></i>
+    <i class="fa-solid fa-circle fa-stack-2x text-white ml-4 scale-90 origin-center"></i>
     <i class="fa-solid fa-pool-8-ball text-black fa-stack-2x ml-4"></i>
 </span>
