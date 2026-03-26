0826f9b9e1908bfb9a6c45c880f4eb9b7ea990eb
John Bell <john@thebiggerboat.co.uk>
2026-03-26 07:50:24 +0000

    Remove public draft status banner

--- resources/views/result/partials/status-banner.blade.php
+++ resources/views/result/partials/status-banner.blade.php
@@ -1,11 +1,3 @@
-@if (! $result->is_confirmed)
-    <div class="rounded-lg border border-yellow-200 bg-yellow-50 px-4 py-3 dark:border-yellow-900/60 dark:bg-yellow-950/30">
-        <p class="text-sm font-medium text-yellow-800 dark:text-yellow-200">
-            This result is still in progress and will remain editable until it is locked.
-        </p>
-    </div>
-@endif
-
 @if ($result->is_overridden)
     <div class="px-4 py-10 text-center sm:px-6">
         <div class="mx-auto max-w-md rounded-xl border border-dashed border-gray-300 px-6 py-8 dark:border-zinc-700 dark:bg-zinc-800/75">
