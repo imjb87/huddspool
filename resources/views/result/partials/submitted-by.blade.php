7c94214d96668d643cc1fc0b5da4692565ce95bf
John Bell <john@thebiggerboat.co.uk>
2026-03-31 06:54:00 +0100

    Adjust result submitted footer spacing

--- resources/views/result/partials/submitted-by.blade.php
+++ resources/views/result/partials/submitted-by.blade.php
@@ -1,5 +1,5 @@
 @if ($result->is_confirmed && $result->submittedBy)
-    <div class="ui-card-footer border-t-0 pt-0 dark:border-t-0">
+    <div class="ui-card-footer">
         <p class="text-xs text-gray-500 dark:text-gray-400">
             Submitted by {{ $result->submittedBy->name }} on {{ $submittedAt->format('j M Y') }} at {{ $submittedAt->format('H:i') }}.
         </p>
