d089b3c9034b0c8fcf94e57f7bafdd8c8432f9e4
John Bell <john@thebiggerboat.co.uk>
2026-04-03 10:09:40 +0100

    Fix header notifications store on standard pages

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
+import './bootstrap';
 import Alpine from 'alpinejs';
 import { bootDeferredGoogleAnalytics } from './google-analytics';
 
 window.Alpine = Alpine;
+window.registerHeaderNotificationsStore?.(Alpine);
 
 Alpine.start();
 bootDeferredGoogleAnalytics();
